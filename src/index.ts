be974838bbcbf64023b3b812fb2567d69adbb465
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-08-20 16:44:33 +0430

    chore: clean code

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,9 @@
 import { writeFileSync, existsSync, mkdirSync, readFileSync } from "fs";
 import { format } from "prettier";
-import {
-  getPathParams,
-  getQueryParams,
-  generateServiceName,
-  getTsType,
-  getHeaderParams,
-} from "./utils";
-import {
-  SwaggerSchemas,
-  SwaggerRequest,
-  SwaggerJson,
-  Schema,
-  SwaggerResponse,
-  SwaggerConfig,
-} from "./types";
-import {
-  HTTP_REQUEST,
-  SERVICE_BEGINNING,
-  CONFIG,
-  DEPRECATED_WARM_MESSAGE,
-} from "./strings";
+import { SwaggerJson, SwaggerConfig } from "./types";
+import { HTTP_REQUEST, CONFIG } from "./strings";
 import { getSwaggerJson } from "./getJson";
+import { generator } from "./generator";
 
 function getParam(name: string): string {
   name += "=";
@@ -31,7 +13,6 @@ function getParam(name: string): string {
 }
 
 async function generate() {
-  let code = SERVICE_BEGINNING;
   const configUrl = getParam("config") || "./swaggerConfig.json";
   let config: SwaggerConfig;
 
@@ -66,143 +47,8 @@ async function generate() {
 
   try {
     const input: SwaggerJson = await getSwaggerJson(url);
-    // JSON.parse(
-    //   readFileSync("./swagger.json", "utf8"),
-    // ); // Input can be any JS object (OpenAPI format)
-    //   const output = swaggerToTS(input); // Outputs TypeScript defs as a string (to be parsed, or written to a file)
 
-    Object.entries(input.paths).forEach(([endPoint, value]) => {
-      Object.entries(value).forEach(
-        ([method, options]: [string, SwaggerRequest]) => {
-          const serviceName = `${method}${generateServiceName(endPoint)}`;
-          const pathParams = getPathParams(options.parameters);
-          const { params: queryParams, hasNullable } = getQueryParams(
-            options.parameters,
-          );
-
-          const {
-            params: headerParams,
-            hasNullable: hasNullableHeaderParams,
-          } = getHeaderParams(options.parameters, config);
-
-          const requestBody = getBodyContent(options.requestBody);
-
-          const contentType = Object.keys(
-            options.requestBody?.content || {
-              "application/json": null,
-            },
-          )[0];
-
-          const accept = Object.keys(
-            options.responses?.[200].content || {
-              "application/json": null,
-            },
-          )[0];
-
-          const responses = getBodyContent(options.responses?.[200]);
-
-          let pathParamsRefString = pathParams.reduce(
-            (prev, { name }) => `${prev}${name},`,
-            "{",
-          );
-          pathParamsRefString = pathParamsRefString
-            ? pathParamsRefString + "}"
-            : "";
-
-          code += `
-/**
- * ${options.summary}
- ${options.deprecated ? `* @deprecated ${DEPRECATED_WARM_MESSAGE}` : ""}
- */
-export const ${serviceName}${options.deprecated ? ": any" : ""} = async (
-    ${pathParams
-      .map(({ name, required, schema }) =>
-        getDefineParam(name, required, schema),
-      )
-      .join(",")}
-    ${pathParams.length > 0 ? "," : ""}
-    ${
-      queryParams
-        ? `${getParamString("queryParams", !hasNullable, queryParams)},`
-        : ""
-    }
-    ${requestBody ? `${getDefineParam("requestBody", true, requestBody)},` : ""}
-    ${
-      headerParams
-        ? `${getParamString(
-            "headerParams",
-            !hasNullableHeaderParams,
-            headerParams,
-          )},`
-        : ""
-    }
-    configOverride?:AxiosRequestConfig
-): Promise<SwaggerResponse<${responses ? getTsType(responses) : "any"}>> => {
-  ${
-    options.deprecated
-      ? `
-  if (__DEV__) {
-    console.warn(
-      "${serviceName}",
-      "${DEPRECATED_WARM_MESSAGE}",
-    );
-  }`
-      : ""
-  }
-  return await responseWrapper(await Http.${method}Request(
-    template("${endPoint}",${pathParamsRefString}),
-    ${queryParams ? "queryParams" : "undefined"},
-    ${requestBody ? "requestBody" : "undefined"},
-    overrideConfig({
-        headers: {
-          "Content-Type": "${contentType}",
-          Accept: "${accept}",
-          ${headerParams ? "...headerParams," : ""}
-        },
-      },
-      configOverride,
-    )
-  ))
-}
-`;
-        },
-      );
-    });
-
-    Object.entries(
-      (input.components.schemas as unknown) as SwaggerSchemas,
-    ).forEach(([name, schema]) => {
-      const { type, enum: Enum, allOf, oneOf } = schema;
-      if (type === "object") {
-        const typeObject = getTsType(schema);
-
-        code += `
-export interface ${name} ${typeObject}
-        `;
-      }
-      if (Enum) {
-        code += `
-export enum ${name} {${Enum.map(
-          (e) => `${e}=${typeof e === "string" ? `"${e}"` : ""}`,
-        )}}
-`;
-      }
-
-      if (allOf) {
-        code += `
-        export interface ${name} extends ${allOf
-          .map((_schema) => getTsType(_schema))
-          .join(" ")}
-                `;
-      }
-      if (oneOf) {
-        code += `
-        export type ${name} = ${oneOf
-          .map((_schema) => getTsType(_schema))
-          .join(" | ")}
-                `;
-      }
-    });
+    const code = generator(input, config);
 
     writeFileSync(`${dir}/services.ts`, format(code, prettierOptions as any));
 
@@ -217,30 +63,6 @@ export enum ${name} {${Enum.map(
   } catch (error) {
     console.log({ error });
   }
-}
-
-function getBodyContent(responses?: SwaggerResponse) {
-  if (!responses) {
-    return responses;
-  }
-
-  return Object.values(responses.content)[0].schema;
-}
-
-function getDefineParam(
-  name: string,
-  required: boolean = false,
-  schema: Schema,
-): string {
-  return getParamString(name, required, getTsType(schema));
-}
-
-function getParamString(
-  name: string,
-  required: boolean = false,
-  type: any,
-): string {
-  return `${name}${required ? "" : "?"}: ${type}`;
 }
 
 generate();
