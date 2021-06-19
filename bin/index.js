85ac956bcbae733b3bd11b9a840574b9ccef9e23
Hossein Mohammadi <hosseinm.developer@gmail.com>
2021-06-19 20:34:51 +0430

    fix: yargs

--- bin/index.js
+++ bin/index.js
@@ -4,30 +4,8 @@ const { generate } = require("../lib");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 
-const argv = yargs(hideBin(process.argv)).command(
-  "swag-ts [tag]",
-  true,
-  (yarg) => {
-    return yarg.positional("tag", {
-      describe: "Just a tag will be updated",
-      default: "",
-    });
-  },
-  (argv) => {
-    generate(undefined, {
-      tag: argv._?.[0],
-    });
-  },
-).argv;
+const argv = yargs(hideBin(process.argv)).argv;
 
-if (argv.internal) {
-  generate(undefined, {
-    tag: argv._?.[0],
-  });
-}
-console.log(argv);
-
-//   .option("tag", {
-//     type: "string",
-//     description: "Update a Tag endpoints and related types",
-//   })
+generate(undefined, {
+  tag: argv._?.[0],
+});
