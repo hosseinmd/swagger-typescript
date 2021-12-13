ad80ffab2a2193aad6df81612a5b352e49db74ef
Hossein Mohammadi <hosseinm.developer@gmail.com>
2021-12-13 11:33:42 +0330

    fix: ignore tls auth

--- src/getJson.ts
+++ src/getJson.ts
@@ -1,6 +1,7 @@
 import Axios from "axios";
-
 async function getJson(url: string) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
   const { data } = await Axios.get(url);
   // fs.writeFileSync("swagger.json", JSON.stringify(data));
 
