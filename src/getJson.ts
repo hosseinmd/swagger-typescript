e2076bf2a1e4cd32f9f4c2f40ec585e919c0f344
Hossein Mohammadi <hosseinm.developer@gmail.com>
2022-02-04 11:11:55 +0330

    feat: support yaml files

--- src/getJson.ts
+++ src/getJson.ts
@@ -1,11 +1,16 @@
 import Axios from "axios";
+import yaml from "js-yaml";
+
 async function getJson(url: string) {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
   const { data } = await Axios.get(url);
-  // fs.writeFileSync("swagger.json", JSON.stringify(data));
 
-  return data;
+  // if url is yaml file convert it to json
+  if (data.hasOwnProperty("swagger")) {
+    return data;
+  }
+  return yaml.load(data);
 }
 
 export { getJson };
